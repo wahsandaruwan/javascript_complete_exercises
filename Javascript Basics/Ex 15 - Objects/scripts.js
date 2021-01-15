9ce588436fa46d44f1d9b58aaa7ed406485ade2d
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-01-15 13:22:31 +0530

    added objects

---
+++ Javascript Basics/Ex 15 - Objects/scripts.js
@@ -0,0 +1,70 @@
+// ----------------------
+// --------Objects-------
+// ----------------------
+
+/*
+    In Javascript objects are variables that has many values.
+    Those values written as name:value pairs called properties or methods.
+*/
+
+// --Object Literals--
+// Comma-separated list of name-value pairs wrapped in curly braces.
+var nimal = {
+    firstName: 'Nimal',
+    lastName: 'Perera',
+    age: 24,
+    isMarried: false,
+    job: 'Developer',
+    subjects: ['Java Programming', 'Web Development', 'Graphic Design', 'Database Management'],
+    degree: {
+        name: 'Software Eng.',
+        duration: '4 years',
+        credits: 120
+    },
+    projects: [
+        {
+            name: 'Ecomerce App',
+            technology: 'Flutter',
+            version: 1.1
+        },
+        {
+            name: 'Music Player',
+            technology: 'Java',
+            version: 1.2
+        },
+        {
+            name: 'POS',
+            technology: 'C#',
+            version: 2.0
+        }
+    ],
+    dateOfBirth: function(year){
+        return year-this.age;
+    }
+}
+
+// Get Values
+console.log(nimal);
+console.log(nimal.firstName);
+console.log(nimal['lastName']);
+var x = 'age';
+console.log(nimal[x]);
+console.log(nimal.subjects[2]);
+console.log(nimal['subjects'][3]);
+console.log(nimal.degree.name);
+console.log(nimal['degree'].credits);
+console.log(nimal.projects[1].technology);
+console.log('Date of Birth : '+nimal.dateOfBirth(2021));
+
+// Modify Values
+nimal.job = 'Engineer';
+nimal['isMarried'] = true;
+console.log(nimal);
+
+// --new Objetc syntax--
+var kasun = new Object();
+kasun.firstName = 'Kasun';
+kasun.lastName = 'Fernando';
+kasun['age'] = 30;
+
+console.log(kasun);
